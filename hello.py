65916759d719fd30ec28f692de25433415cd91c0
odulaja philip <odulajaphilip@gmail.com>
2022-07-16 00:19:34 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+import sys
+from django.conf import settings
+from django.conf.urls import patterns
+from django.core.management import execute_from_command_line
+from django.http import HttpResponse
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='thisisabadkeybutitwilldo',
+    ROOT_URLCONF=sys.modules[__name__],
+)
+
+
+def index(request):
+    return HttpResponse('Hello, World')
+
+
+urlpatterns = patterns('',(r'^hello-world/$', index),)
+
+
+if __name__ == "__main__":
+    execute_from_command_line(sys.argv)
